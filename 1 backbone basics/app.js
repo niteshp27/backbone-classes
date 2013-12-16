86cff1c70feb021b7ae5b6e920615dcea40f9382
nitesh <niteshp27@gmail.com>
2013-12-16 16:39:49 +0530

    class no 2

--- 1 backbone basics/app.js
+++ 1 backbone basics/app.js
@@ -1,14 +1,55 @@
 ﻿
-//defining a model
-var Todo = Backbone.Model.extend({
-	defaults: { title:'', completed: false}
+//1 defining a model
+var Todomodel = Backbone.Model.extend({
+	defaults: { title:'', completed: false},
+	initialize: function(){
+      console.log('This model has been initialized 1.');
+  }
 });
 
-//instantiate todo model
-var myTodo = new Todo({
+//2 instantiate todo model
+var Todo1 = new Todomodel({
 	title:'check'
 });
 
+var Todo2 = new Todomodel({
+	title:'Todo 2'
+});
+
+//getters
+console.log(Todo2.get('title')); // Todo 2 string
+console.log(Todo2.get('completed')); // false
+
+//setters
+// Set single attribute value at a time through Model.set():
+Todo2.set("title", "Title attribute set through Model.set().");
+console.log('Todo title: ' + Todo2.get('title')); // Todo title: Title attribute set through Model.set().
+console.log('Completed: ' + Todo2.get('completed')); // Completed: false
+
+// Set map of attributes through Model.set():
+Todo2.set({
+  title: "Both attributes set through Model.set().",
+  completed: true
+});
+console.log('Todo title: ' + Todo2.get('title')); // Todo title: Both attributes set through Model.set().
+console.log('Completed: ' + Todo2.get('completed')); // Completed: true
+
+
+
+
+
+//tojson to read data,it returns-> object -> tostringify returns string value instead of object
+// Following logs: {"title":"Todo 2","completed":true} 
+console.log(JSON.stringify(Todo2));
+
+//returns json object
+var todo1Attributes = Todo2.toJSON();
+// Following logs: {"title":"Todo 2","completed":false} 
+console.log(todo1Attributes);
+
+
+
+//3 extend view
 var TodoView = Backbone.View.extend({
 
   tagName:  'li',
@@ -33,7 +74,7 @@ var TodoView = Backbone.View.extend({
 
   // Re-render the titles of the todo item.
   render: function() {
-    this.$el.html( this.todoTpl( this.model.toJSON() ) );
+    this.$el.append( this.todoTpl( this.model.toJSON() ) );
     // $el here is a reference to the jQuery element 
     // associated with the view, todoTpl is a reference
     // to an Underscore template and toJSON() returns an 
@@ -60,7 +101,8 @@ var TodoView = Backbone.View.extend({
 });
 
 // create a view for a todo
-var todoView = new TodoView({model: myTodo});
+var todoView1 = new TodoView({model: Todo1});
+var todoView2 = new TodoView({model: Todo2});
 
-
-todoView.render();
+todoView1.render();
+todoView2.render();
